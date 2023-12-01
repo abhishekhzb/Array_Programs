7d6d1848c4b5cb00e26b9cfec8e9a38bf7a200a7
Abhishek kumar <abhishekhzb8@gmail.com>
2023-12-01 12:22:58 +0530

    Create FindArrayDuplicatesByIndex.java

---
+++ FindArrayDuplicatesByIndex.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+// Using HashSet removing duplicates.
+
+public class FindArrayDuplicatesByIndex {
+    public static void main(String[] args) {
+    
+    int[] a = {1,2,3,4,5,3,4,5};
+    
+    for(int i = 0;i<a.length;i++){
+      for(int j = i+1;j<a.length;j++){
+        if(a[i] == a[j]){
+          System.out.println("Duplicates is : "+a[i]);
+        }
+      }
+    }
+    
+  } 
+}
