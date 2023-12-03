be7c8947799c630fd2ae7c13c7de8303152468ad
Abhishek kumar <abhishekhzb8@gmail.com>
2023-12-03 21:11:23 +0530

    Create SortArrayInbuiltFunction.java

---
+++ SortArrayInbuiltFunction.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class SortArrayInbuiltFunction {
+    public static void main(String[] args) {
+      
+      int[] arr = {1,2,3,4,5,6,4,3,2,1};
+      
+      Arrays.parallelSort(arr);
+      
+      System.out.println(Arrays.toString(arr));
+      
+      
+  }
+}
